b3be4f94d67d993757ffd108792345bd88c34bce
aktherhossen01 <mdakterhossen122@gmail.com>
2026-01-05 12:49:18 +0600

    data base connected

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,47 @@
 import express, { Request, Response } from "express"
+import {Pool} from "pg"
 const app = express()
 const port = 5000
 
+app.use(express.json())
+
+const pool = new Pool({
+  connectionString:`postgresql://neondb_owner:npg_kvgZi9mQjP4G@ep-steep-math-a4fgrg11-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
+})
+
+
+const initDB = async()=>{
+  await pool.query( `
+    CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    age INT,
+    phone VARCHAR(15),
+    address TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    )
+    
+     `)
+     await pool.query(`
+      
+      `)
+};
+initDB();
+
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
+})
+
+app.post("/",(req:Request,res:Response)=>{
+  console.log(req.body);
+
+  res.status(201).json({
+    success:true,
+    message:"API is working"
+  })
+  
 })
 
 app.listen(port, () => {
