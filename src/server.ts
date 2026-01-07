2c0d984f4bf0bbe1200b300922b34dbc03db6c68
Md Akter Hosen <mdakterhossen122@gmail.com>
2026-01-07 22:55:33 +0600

    get post route create

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 import path from "path"
 
 
+
 dotenv.config({path: path.join(process.cwd(),".env")})
 const app = express()
 const port = 5000
@@ -49,6 +50,8 @@ app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
 })
 
+
+// user crud
 app.post("/users",async(req:Request,res:Response)=>{
   const {name,email}=req.body;
 
@@ -69,14 +72,26 @@ app.post("/users",async(req:Request,res:Response)=>{
       message:err.message
     })
   }
-
-  res.status(201).json({
-    success:true,
-    message:"API is working"
-  })
-  
 })
 
+
+app.get("/users",async(req:Request,res:Response)=>{
+
+  try {
+    const result = await pool.query(`SELECT * FROM users`);
+    res.status(200).json({
+      success:true,
+      message:"Users created",
+      data:result.rows
+    })
+    
+  } catch (err:any) {
+    res.status(500).json({
+      success:false,
+      message:err.message
+    })
+  }
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
